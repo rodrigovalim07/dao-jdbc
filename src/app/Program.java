a013d5a899c3b6f1b85109e9af38acffa80a633e
Rodrigo Valim <rodrigovalimjr2011@hotmail.com>
2024-04-25 17:23:08 -0300

    feito a classe Department

---
+++ src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "Livros");
+		System.out.println(obj);
+		
+	}
+
+}
